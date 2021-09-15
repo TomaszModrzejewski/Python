2529dd08c7fa405572e93ce7b3ecb212d62d0605
Goh <84911945+TomaszModrzejewski@users.noreply.github.com>
2021-09-15 23:15:58 +0200

    Add files via upload

---
+++ file.py
@@ -0,0 +1,9 @@
+
+a=str(input("Enter the name of the file with .txt extension:"))
+file2=open(a,'r')
+line=file2.readline()
+while(line!=""):
+    print(line)
+    line=file2.readline()
+file2.close()	
+	
