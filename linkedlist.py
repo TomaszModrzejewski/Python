f7dbec5308bbbf04f932db6f619cbba60fd2eeb8
Goh <84911945+TomaszModrzejewski@users.noreply.github.com>
2021-09-19 21:01:04 +0200

    linkedlist

---
+++ linkedlist.py
@@ -0,0 +1,42 @@
+
+class Node:
+    # Singly linked node
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+class singly_linked_list:
+    def __init__(self):
+        # Createe an empty list
+        self.tail = None
+        self.head = None
+        self.count = 0
+
+    def iterate_item(self):
+        # Iterate the list.
+        current_item = self.tail
+        while current_item:
+            val = current_item.data
+            current_item = current_item.next
+            yield val
+
+    def append_item(self, data):
+        #Append items on the list
+        node = Node(data)
+        if self.head:
+            self.head.next = node
+            self.head = node
+        else:
+            self.tail = node
+            self.head = node
+        self.count += 1
+
+items = singly_linked_list()
+items.append_item('PHP')
+items.append_item('Python')
+items.append_item('C#')
+items.append_item('C++')
+items.append_item('Java')
+
+for val in items.iterate_item():
+    print(val)	
+	
