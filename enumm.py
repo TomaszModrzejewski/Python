175271e453219b5afa08eeb26abd232d87836916
Goh <84911945+TomaszModrzejewski@users.noreply.github.com>
2021-09-23 23:17:40 +0200

    enum

---
+++ enumm.py
@@ -0,0 +1,12 @@
+
+from enum import Enum
+class Country(Enum):
+    Afghanistan = 93
+    Albania = 355
+    Algeria = 213
+    Andorra = 376
+    Angola = 244
+    Antarctica = 672
+print('\nMember name: {}'.format(Country.Albania.name))
+print('Member value: {}'.format(Country.Albania.value))	
+	
