d33b5764cf8181ade799a0f170dab205d1014b70
Goh <84911945+TomaszModrzejewski@users.noreply.github.com>
2021-09-28 06:38:50 +0200

    binary

---
+++ binary.py
@@ -0,0 +1,15 @@
+
+l=[]
+def convert(b):
+    if(b==0):
+        return l
+    dig=b%2
+    l.append(dig)
+    convert(b//2)
+a=int(input("Enter a number: "))
+convert(a)
+l.reverse()
+print("Binary equivalent:")
+for i in l:
+    print(i)	
+	
