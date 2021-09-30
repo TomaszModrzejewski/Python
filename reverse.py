c4794e1b15f2ae562f2855fbb3aa9779314ce953
Goh <84911945+TomaszModrzejewski@users.noreply.github.com>
2021-09-30 04:55:02 +0200

    reverse

---
+++ reverse.py
@@ -0,0 +1,12 @@
+
+def string_reverse(str1):
+
+    rstr1 = ''
+    index = len(str1)
+    while index > 0:
+        rstr1 += str1[ index - 1 ]
+        index = index - 1
+    return rstr1
+print(string_reverse('1234abcd'))
+				 
+	
