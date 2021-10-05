c686ea67413fb622d846d5c0172fd888ea5a2568
Goh <84911945+TomaszModrzejewski@users.noreply.github.com>
2021-10-05 20:13:57 +0200

    Update password_manager.py

--- Passwords/password_manager.py
+++ Passwords/password_manager.py
@@ -1,10 +1,10 @@
 from cryptography.fernet import Fernet
 
-'''
-def write_key():
-    key = Fernet.generate_key()
-    with open("key.key", "wb") as key_file:
-        key_file.write(key)'''
+
+#def write_key():
+    #key = Fernet.generate_key()
+    #with open("key.key", "wb") as key_file:
+        #key_file.write(key)
 
 
 def load_key():
