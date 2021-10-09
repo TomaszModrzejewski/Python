61ddfcce9cb6f07d607513a2e70d715b1ce00581
Goh <84911945+TomaszModrzejewski@users.noreply.github.com>
2021-10-09 22:20:20 +0200

    Update dict1.py

--- Email ID Dictionary/dict1.py
+++ Email ID Dictionary/dict1.py
@@ -14,16 +14,12 @@ for line in fh:
 freq_mail = max(mails, key=mails.count)  # To find frequent mail
 print(freq_mail, mails.count(freq_mail))  # To find countof frequent mail
 
-
-"""
-for x in mails:
-    counts[x]=counts.get(x,0)+1
-bigmail=None
-bigvalue=None
-for key,value in counts.items():
-    if bigvalue==None or bigvalue<value:
-        bigmail=key
-        bigvalue=value
-print(bigmail, bigvalue)
-
-"""
+#for x in mails:
+    #counts[x]=counts.get(x,0)+1
+#bigmail=None
+#bigvalue=None
+#for key,value in counts.items():
+    #if bigvalue==None or bigvalue<value:
+        #bigmail=key
+        #bigvalue=value
+#print(bigmail, bigvalue)
