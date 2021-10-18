cff7ebc392236e30f96f062edf1730e184cece51
Goh <84911945+TomaszModrzejewski@users.noreply.github.com>
2021-10-18 16:25:52 +0200

    Create IPython-core1.py

---
+++ IPython-core1.py
@@ -0,0 +1,46 @@
+# This code can be put in any Python module, it does not require IPython
+# itself to be running already.  It only creates the magics subclass but
+# doesn't instantiate it yet.
+from __future__ import print_function
+from IPython.core.magic import (Magics, magics_class, line_magic,
+                                cell_magic, line_cell_magic)
+
+# The class MUST call this class decorator at creation time
+@magics_class
+class MyMagics(Magics):
+
+    @line_magic
+    def lmagic(self, line):
+        "my line magic"
+        print("Full access to the main IPython object:", self.shell)
+        print("Variables in the user namespace:", list(self.shell.user_ns.keys()))
+        return line
+
+    @cell_magic
+    def cmagic(self, line, cell):
+        "my cell magic"
+        return line, cell
+
+    @line_cell_magic
+    def lcmagic(self, line, cell=None):
+        "Magic that works both as %lcmagic and as %%lcmagic"
+        if cell is None:
+            print("Called as line magic")
+            return line
+        else:
+            print("Called as cell magic")
+            return line, cell
+
+
+# In order to actually use these magics, you must register them with a
+# running IPython.
+
+def load_ipython_extension(ipython):
+    """
+    Any module file that define a function named `load_ipython_extension`
+    can be loaded via `%load_ext module.path` or be configured to be
+    autoloaded by IPython at startup time.
+    """
+    # You can register the class itself without instantiating it.  IPython will
+    # call the default constructor on it.
+    ipython.register_magics(MyMagics)
