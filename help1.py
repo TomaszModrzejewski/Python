722671e109616357a9c071b80c574d79a4d5ff9e
Goh <84911945+TomaszModrzejewski@users.noreply.github.com>
2021-11-11 15:58:43 +0100

    help

---
+++ help1.py
@@ -0,0 +1,22 @@
+
+import heapq
+from pprint import pprint
+items = [
+    {'name': 'Item-1', 'price': 101.1},
+    {'name': 'Item-2', 'price': 555.22},
+    {'name': 'Item-3', 'price': 45.09},
+    {'name': 'Item-4', 'price': 22.75},
+    {'name': 'Item-5', 'price': 16.30},
+    {'name': 'Item-6', 'price': 110.65}
+]
+
+cheap = heapq.nsmallest(3, items, key=lambda s: s['price'])
+expensive = heapq.nlargest(3, items, key=lambda s: s['price'])
+print("Original datasets:")
+pprint(items)
+print("\nFirst 3 expensive items:")
+pprint(expensive)
+print("\nFirst 3 cheap items:")
+pprint(cheap)
+				 
+	
